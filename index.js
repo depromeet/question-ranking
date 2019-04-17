b286d24dd13694cf32d78e56a752c0c202c70843
yongseongjeon <yongseong.jeon95@gmail.com>
2019-04-17 16:09:57 +0900

    모바일 입력창 하단에 고정

--- index.js
+++ index.js
@@ -1,55 +1,84 @@
-function changeRankingHeight() {
-    $( ".Question-ranking-more" ).hide();
-    $( ".fold-button" ).hide();
+const main = function changeRankingHeight(foldedHeight, strechedHeight) {
+    const $questionRanking = $('.Question-ranking');
+    const $questionRankingMore = $('.Question-ranking-more');
+    const $questionContents = $('.question-contents');
+    const $foldButton = $('.fold-button');
+    const $passDownButton = $('.pass-down-button');
+    const $passUpButton = $('.pass-up-button');
+
+    $questionRankingMore.hide();
+    $foldButton.hide();
     
-    $( ".pass-down-button" ).click(function(){
-        $( ".Question-ranking" ).hide();
-        $( ".Question-ranking-more" ).show();
-        $( ".fold-button" ).show();
-        $( ".question-contents" ).css( "height", "702px" );
+    $passDownButton.click(function(){
+        $questionRanking.hide();
+        $questionRankingMore.show();
+        $foldButton.show();
+        $questionContents.css({
+            'height': strechedHeight,
+        });
     });
     
-    $( ".pass-up-button" ).click(function(){
-        $( ".Question-ranking" ).show();
-        $( ".Question-ranking-more" ).hide();
-        $( ".fold-button" ).hide();
-        $( ".question-contents" ).css( "height", "848px" );
+    $passUpButton.click(function(){
+        $questionRanking.show();
+        $questionRankingMore.hide();
+        $foldButton.hide();
+        $questionContents.css({
+            'height': foldedHeight,
+        });
     });
 }
 
-function mobileChangeRankingHeight(mobileStrechedHeight, mobileFoldedHeight) {
-    $( ".mobile-circle-button" ).click(function(){
-        $( ".Question-ranking" ).hide();
-        $( ".Question-ranking-more" ).show();
-        $( ".question-contents" ).css( "height", mobileStrechedHeight );
+const mobile = function mobileChangeRankingHeight(foldedHeight, strechedHeight) {
+    const $questionRanking = $('.Question-ranking');
+    const $questionRankingMore = $('.Question-ranking-more');
+    const $questionContents = $('.question-contents');
+    const $circleButton = $('.mobile-circle-button');
+    const $secondCircleButton = $('.mobile-circle-button-2')
+
+    $circleButton.click(function() {
+        $questionRanking.hide();
+        $questionRankingMore.show();
+        $questionContents.css({
+            'height': strechedHeight,
+        });
     });
 
-    $( ".mobile-circle-button-2" ).click(function(){
-        $( ".Question-ranking" ).show();
-        $( ".Question-ranking-more" ).hide();
-        $( ".question-contents" ).css( "height", mobileFoldedHeight );
+    $secondCircleButton.click(function() {
+        $questionRanking.show();
+        $questionRankingMore.hide();
+        $questionContents.css({
+            'height': foldedHeight,
+        });
     });
 }
 
-function showQRcode() {
-    $( ".box-5-title-3" ).click(function(){
-        $( ".modal" ).show();
+const QRcode = function showOrHideQuickResponsiveCode() {
+    const $quickResponsiveCode = $('.box-5-title-3');
+    const $modal = $('.modal');
+
+    $quickResponsiveCode.click(function() {
+        $modal.show();
     });
-    $( ".modal" ).click(function(){
-        $( ".modal" ).hide();
+    $modal.click(function() {
+        $modal.hide();
     });
 }
 
 $(function () {
-    const contnetsHeight = $( ".question-contents" ).css( "height" );
-    const mobileFoldedHeight = (parseInt(contnetsHeight.replace(/px/,""))-218)+"px";
-    const mobileStrechedHeight = (parseInt(contnetsHeight.replace(/px/,""))-364)+"px";
+    const currentHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const currentWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    const mobileFoldedHeight = parseInt(currentHeight-110)+"px";
+    const mobileStrechedHeight = parseInt(currentHeight-256)+"px";
+    const mainFoldedHeight = parseInt(currentHeight-232)+"px";
+    const mainStrechedHeight = parseInt(currentHeight-378)+"px";
 
-    if ( Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 425 ) {
+    if ( currentWidth < 425 ) {
         $( ".question-contents" ).css( "height", mobileFoldedHeight );
-    };
+    } else {
+        $( ".question-contents" ).css( "height", mainFoldedHeight );
+    }
     
-    changeRankingHeight();
-    mobileChangeRankingHeight(mobileStrechedHeight, mobileFoldedHeight);
-    showQRcode();
+    main(mainFoldedHeight, mainStrechedHeight);
+    mobile(mobileFoldedHeight, mobileStrechedHeight);
+    QRcode();
 });
